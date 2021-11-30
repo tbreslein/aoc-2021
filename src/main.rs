e499d5a5577428e08b089f1039766f733160ee83
Tommy Breslein <tommy.breslein@protonmail.com>
2021-11-30 19:45:43 +0100

    add clap functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+
+/// Oh, hi...
+#[derive(Parser)]
+#[clap(version = "0.0.1", author = "tommyb")]
+struct Opts {
+    /// which day of the AoC you wanna run; valid values: {1, 2, ..}
+    #[clap(short, long)]
+    day: i32,
+
+    /// which part of that day you wanna run; valid values: {1, 2}
+    #[clap(short, long)]
+    part: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("Value for day: {}", opts.day);
+    println!("Value for part: {}", opts.part);
+
+    match opts.day {
+        1 => {match opts.part {
+            1 => println!("Day 1, part 1!"),
+            2 => println!("Day 1, part 2!"),
+            _ => println!("You chose part {}, but there only 2 parts per day!", opts.part),
+        }},
+        _ => println!("Have not finished day {} (yet)...", opts.day),
+    };
 }
