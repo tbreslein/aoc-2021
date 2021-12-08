97bac70826bc9cf91b57e00efdfe71627c4c0dcf
T. Breslein <tbreslein@astrophysik.uni-kiel.de>
2021-12-08 09:35:02 +0100

    rename solution submodules

--- src/common/pick_challenge.rs
+++ src/common/pick_challenge.rs
@@ -6,44 +6,44 @@ pub fn pick_challenge(day: i32, part: i32, file: String) -> Result<(), Box<dyn E
     let data = fs::read_to_string(file)?;
     match day {
         1 => match part {
-            1 => println!("result = {}", solutions::day1::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day1::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day01::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day01::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
         2 => match part {
-            1 => println!("result = {}", solutions::day2::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day2::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day02::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day02::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
         3 => match part {
-            1 => println!("result = {}", solutions::day3::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day3::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day03::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day03::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
         4 => match part {
-            1 => println!("result = {}", solutions::day4::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day4::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day04::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day04::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
         5 => match part {
-            1 => println!("result = {}", solutions::day5::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day5::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day05::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day05::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
         6 => match part {
-            1 => println!("result = {}", solutions::day6::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day6::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day06::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day06::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
         7 => match part {
-            1 => println!("result = {}", solutions::day7::solve_p1(&data)),
-            2 => println!("result = {}", solutions::day7::solve_p2(&data)),
+            1 => println!("result = {}", solutions::day07::solve_p1(&data)),
+            2 => println!("result = {}", solutions::day07::solve_p2(&data)),
             _ => println!("You chose part {}, but there only 2 parts per day!", part),
         },
 
--- src/solutions.rs
+++ src/solutions.rs
@@ -1,7 +1,7 @@
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
